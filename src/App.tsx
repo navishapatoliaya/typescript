ae47f98850bb84b12d704f9de81ccd84a31ea2f2
MAC <mac@DREAMSs-iMac-Pro.local>
2021-08-09 18:06:57 +0530

    addlogin-register

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import './App.css';
+import { LoginPage } from './LoginPage';
+import { RegisterPage } from './RegistrePage';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Router>
+          <Switch>
+            <Route path ="/login" component={LoginPage} />
+            <Route path="/register" component={RegisterPage} />
+          </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/RegistrePage/RegisterPage.tsx
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+interface RegisterProp{
+    name?: any;
+    value?: any;
+    register?: any;
+    registering: any;
+    submitted:any;
+    user: string
+
+}
+interface Fields{
+    firstName?: string,
+    lastName?: string,
+    userName?: string,
+    password?: string   
+}
+interface RegisterState{
+   
+    submitted :boolean;
+    user?: Fields;
+
+}
+
+export class RegisterPage extends Component <RegisterProp,RegisterState>{
+    constructor(props :RegisterProp){
+        super(props);
+        this.state={
+            user:{
+                firstName : '',
+                lastName : '',
+                userName : '',
+                password : '',
+            },
+            submitted:false
+        };
+    }  
+    
+        handleChange = (event: any)=>{
+            this.setState({user: {
+                    ...this.state.user,
+                    [event?.target.name]: event?.target.value
+                }});
+            // this.setState(Object.assign(this.state,{user,[name]:value}));
+        }
+
+    // handleSubmit(event:any) {
+    //     event.preventDefault();
+
+    //     this.setState({ submitted: true });
+    //     const { user } = this.state;
+    //     if (user.firstName && user.lastName && user.username && user.password) {
+    //         this.props.register(user);
+    //     }
+    // }
+    render() {
+        const {registering} =this.props;
+        const {user,submitted} =this.state;
+        return (
+            <div>
+
+                <h1>Register</h1>
+                <form name="form">
+                    <div className={"form-group" + (submitted && !user?.firstName ? ' has-error' : '') }>
+                        <label htmlFor="firstName">First Name</label>
+                        <input type="text" className="form-control" name="firstName" value={user?.firstName} onChange={this.handleChange}  />
+                        {/* {submitted && !user.firstName &&
+                            <div className="help-block">First Name is required</div>
+                        } */}
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="lastname">Last Name:</label>
+                        <input type="text" className="form-control" name="lastname" value={user?.lastName} onChange={this.handleChange} />
+                        {/* {submitted && !user.lastName &&
+                            <div className="help-block">Last Name is required</div>
+                        } */}
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="username">User Name:</label>
+                        <input type="text" className="form-control" name="username" value={user?.userName} onChange={this.handleChange}/>
+                        {/* {submitted && !user.username &&
+                            <div className="help-block">Username is required</div>
+                        } */}
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password:</label>
+                        <input type="text" className="form-control" name="password" value={user?.password} onChange={this.handleChange}/>
+                        {/* {submitted && !user.password &&
+                            <div className="help-block">Password is required</div>
+                        } */}
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary">Register</button>
+                        <Link to="/login" className="btn btn-link">Cancel</Link>
+
+                    </div>
+
+                </form>
+            </div>
+        )
+    }
+}
+
+export default RegisterPage;
